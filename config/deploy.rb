0c4077bb81d605a15a4a13d6b47f50a8eb1def63
Koji Shimba <me@shimba.co>
2016-01-15 00:04:35 +0900

    rake db:seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,4 +38,14 @@ namespace :deploy do
       invoke "puma:restart"
     end
   end
+
+  task :seed do
+    on roles(:app), in: :sequence, wait: 5 do
+      within "#{current_path}" do
+        with rails_env: "#{fetch(:stage)}" do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
 end
